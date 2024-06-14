ea844c0777a0d0d74b712ec2567682fad2dce85f
Jerucho <amadorjeremias2006@gmail.com>
2024-06-14 17:05:00 -0500

    Cambiar el fondo del banner automaticamente

--- scripts/carrusel.js
+++ scripts/carrusel.js
@@ -1,7 +1,20 @@
-const bannerImgs = ["https://cdn2.steamgriddb.com/hero/c73f4d8f3e0c84920eef1464c4c73cb8.jpg","../../images/Banner.png","https://i.redd.it/vlm6uxl72j4c1.png"]
+const bannerImgs = [
+  "../../images/Banner.png",
 
-const banner = document.getElementById("banner")
+  "https://cdn2.steamgriddb.com/hero/c73f4d8f3e0c84920eef1464c4c73cb8.jpg",
 
-setInterval(()=>{
-    banner.style
-}, 2000)
+  "https://i.redd.it/vlm6uxl72j4c1.png",
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+  const banner = document.getElementById("banner");
+  const bannerText = document.getElementById("bannerText");
+  let currentIndex = 0;
+
+  function changeBackground() {
+    banner.style.backgroundImage = `url('${bannerImgs[currentIndex]}')`;
+    currentIndex = (currentIndex + 1) % bannerImgs.length;
+  }
+
+  setInterval(changeBackground, 3000);
+});
