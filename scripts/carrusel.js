196a24e7de4a5aadd47b355b5c76ee06b9696ab5
Jerucho <78826595+Jerucho@users.noreply.github.com>
2024-06-14 10:54:39 -0500

    clase

---
+++ scripts/carrusel.js
@@ -0,0 +1,7 @@
+const bannerImgs = ["https://cdn2.steamgriddb.com/hero/c73f4d8f3e0c84920eef1464c4c73cb8.jpg","../../images/Banner.png","https://i.redd.it/vlm6uxl72j4c1.png"]
+
+const banner = document.getElementById("banner")
+
+setInterval(()=>{
+    banner.style
+}, 2000)
