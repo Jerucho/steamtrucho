b7dcf1b21c73e3019c4631e9602cf2a2e17030af
Jerucho <amadorjeremias2006@gmail.com>
2024-06-27 18:58:46 -0500

    Pagina de promociones agregada

--- pages/Tienda/scripts/juegos.js
+++ pages/Tienda/scripts/juegos.js
@@ -2,11 +2,11 @@ const juegos = [
   {
     titulo: "NBA 2K24",
     precio: "59.99 USD",
-    url_tienda: "https://store.steampowered.com/app/2338770/NBA_2K24/",
+    url_tienda: "https://store.steampowered.com/app/2338770/NBA_2K24/2",
     descuento: "10%",
     imagen:
       "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2023/07/nba-2k24-3076874.jpg",
-    generos: ["Deportes", "Simulación"],
+    generos: ["Deportes", "Simulación", "Multijugador"],
   },
   {
     titulo: "F1 24",
@@ -14,7 +14,7 @@ const juegos = [
     url_tienda: "https://store.steampowered.com/app/2488620/F1_24/",
     descuento: "",
     imagen: "../../images/f1_24.avif",
-    generos: ["Carreras", "Simulación"],
+    generos: ["Carreras", "Simulación", "Multijugador"],
   },
   {
     titulo: "Fifa 24",
@@ -23,7 +23,7 @@ const juegos = [
       "https://store.steampowered.com/app/2195250/EA_SPORTS_FC_24/?l=spanish",
     descuento: "",
     imagen: "https://i.blogs.es/934844/20/450_1000.png",
-    generos: ["Deportes", "Simulación"],
+    generos: ["Deportes", "Simulación", "Multijugador"],
   },
   {
     titulo: "Red Dead Redemption 2",
@@ -59,7 +59,7 @@ const juegos = [
     url_tienda: "https://store.epicgames.com/es-ES/p/ark",
     descuento: "15%",
     imagen: "../../images/ark.jpg",
-    generos: ["Supervivencia", "Mundo Abierto", "Aventura"],
+    generos: ["Supervivencia", "Mundo Abierto", "Aventura", "Multijugador"],
   },
   {
     titulo: "Counter Strike 2",
@@ -101,6 +101,71 @@ const juegos = [
     imagen: "../../images/gof_of_war.jpg",
     generos: ["Acción", "Aventura", "Mitología"],
   },
+  {
+    titulo: "Minecraft",
+    precio: "29.99 USD",
+    url_tienda: "https://www.minecraft.net/es-es/store/minecraft-java-edition",
+    descuento: "",
+    imagen: "../../images/minecraft.jpg",
+    generos: ["Mundo Abierto", "Supervivencia", "Aventura"],
+  },
+  {
+    titulo: "Left 4 Dead 2",
+    precio: "19.99 USD",
+    url_tienda: "https://store.steampowered.com/app/550/Left_4_Dead_2/",
+    descuento: "",
+    imagen: "../../images/left_2.webp",
+    generos: ["FPS", "Zombies", "Survival", "Terror", "Multijugador"],
+  },
+  {
+    titulo: "Left 4 Dead",
+    precio: "19.99 USD",
+    url_tienda: "https://store.steampowered.com/app/500/Left_4_Dead/",
+    descuento: "50%",
+    imagen: "../../images/left.webp",
+    generos: ["FPS", "Zombies", "Survival", "Terror", "Multijugador"],
+  },
+  {
+    titulo: "Mortal Kombat 11",
+    precio: "49.99 USD",
+    url_tienda: "https://store.steampowered.com/app/976310/Mortal_Kombat_11/",
+    descuento: "",
+    imagen: "../../images/mortal_kombat.webp",
+    generos: ["Lucha", "Multijugador", "Aventura"],
+  },
+  {
+    titulo: "The Last of Us 2",
+    precio: "39.99 USD",
+    url_tienda:
+      "https://www.playstation.com/es-pe/games/the-last-of-us-part-ii/",
+    descuento: "",
+    imagen: "../../images/the_last_of_us.webp",
+    generos: ["Acción", "Aventura", "Zombies"],
+  },
+  {
+    titulo: "Spiderman Miles Morales",
+    precio: "49.99 USD",
+    url_tienda: "https://www.playstation.com/es-pe/games/marvels-",
+    descuento: "",
+    imagen: "../../images/spiderman.jpeg",
+    generos: ["Acción", "Aventura", "Superheroes"],
+  },
+  {
+    titulo: "Spiderman",
+    precio: "39.99 USD",
+    url_tienda: "",
+    descuento: "",
+    imagen: "../../images/spiderman_miles.jpeg",
+    generos: ["Acción", "Aventura", "Superheroes"],
+  },
+  {
+    titulo: "Rocket League",
+    precio: "Free to Play",
+    url_tienda: "https://www.rocketleague.com/",
+    descuento: "",
+    imagen: "../../images/rocket_league.jpg",
+    generos: ["Deportes", "Coches", "Multijugador"],
+  },
 ];
 
 // Recuperar el contenedor del HTML
@@ -112,11 +177,13 @@ const mostrarJuegos = (juegosFiltrados) => {
   // Recorremos el array de juegos
   juegosFiltrados.forEach((juego) => {
     const card = document.createElement("div");
-    card.innerHTML = `<div class="tarjeta-juego">
+    card.innerHTML = `
+    <div class="tarjeta-juego">
     <a class="card-a" href=${juego.url_tienda} target="_blank">
             <img
               src=${juego.imagen}
-              alt="Imagen del juego"
+              alt="Imagen del juego ${juego.titulo}" 
+              loading="lazy"
             />
             <div class="juego-descripcion">
                 
@@ -127,7 +194,7 @@ const mostrarJuegos = (juegosFiltrados) => {
                     ? '<div class="descuento-tarjeta">' +
                       juego.descuento +
                       "</div>"
-                    : ""
+                    : "<div></div>"
                 }
                 <span>${juego.precio}</span>
               </div>
